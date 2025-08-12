cbd0c070083219643fca1c34c8271de77833212d
Sávio <saviodz0000@gmail.com>
2025-08-12 20:53:27 -0300

    segundo commit

--- src/atividades/ativiadade14.java
+++ src/atividades/ativiadade14.java
@@ -3,43 +3,33 @@ package atividades;
 import java.util.Scanner;
 
 public class ativiadade14 {
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        double[] numeros = new double[4];
 
-        double[] numeros = new double[4];
-        int positivos = 0;
-        int negativos = 0;
+	        for (int i = 0; i < 4; i++) {
+	            System.out.print("Digite o " + (i + 1) + "º número: ");
+	            numeros[i] = sc.nextDouble();
+	        }
 
-        System.out.println("Digite 4 números reais:");
+	        double maior = numeros[0];
+	        double menor = numeros[0];
+	        int positivos = 0;
+	        int negativos = 0;
 
-        for (int i = 0; i < 4; i++) {
-            numeros[i] = sc.nextDouble();
+	        for (double num : numeros) {
+	            maior = num > maior ? num : maior;
+	            menor = num < menor ? num : menor;
+	            positivos += num > 0 ? 1 : 0;
+	            negativos += num < 0 ? 1 : 0;
+	        }
 
-            if (numeros[i] > 0) {
-                positivos++;
-            } else if (numeros[i] < 0) {
-                negativos++;
-            }
-        }
+	        System.out.println("Maior número: " + maior);
+	        System.out.println("Menor número: " + menor);
+	        System.out.println("Quantidade de positivos: " + positivos);
+	        System.out.println("Quantidade de negativos: " + negativos);
 
-        double maior = numeros[0];
-        double menor = numeros[0];
-
-        for (int i = 1; i < 4; i++) {
-            if (numeros[i] > maior) {
-                maior = numeros[i];
-            }
-            if (numeros[i] < menor) {
-                menor = numeros[i];
-            }
-        }
-
-        System.out.println("Maior número: " + maior);
-        System.out.println("Menor número: " + menor);
-        System.out.println("Positivos: " + positivos);
-        System.out.println("Negativos: " + negativos);
-        sc.close();
-    }
-}
-
+	        sc.close();
+	    }
+	}
 //Leia 4 números reais. Escreva o maior, o menor, quantos são positivos e quantos são negativos.
