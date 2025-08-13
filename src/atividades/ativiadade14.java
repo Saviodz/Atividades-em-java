a8990d5c1f817c3043a4b44a689f4915976f8836
Sávio <saviodz0000@gmail.com>
2025-08-13 13:48:32 -0300

    quarto commit

--- src/atividades/ativiadade14.java
+++ src/atividades/ativiadade14.java
@@ -3,35 +3,43 @@ package atividades;
 import java.util.Scanner;
 
 public class ativiadade14 {
-	    public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        double[] numeros = new double[4];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-	        for (int i = 0; i < 4; i++) {
-	            System.out.print("Digite o " + (i + 1) + "º número: ");
-	            numeros[i] = sc.nextDouble();
-	        }
+		double[] numeros = new double[4];
 
-	        double maior = numeros[0];
-	        double menor = numeros[0];
-	        int positivos = 0;
-	        int negativos = 0;
+		int positivos = 0;
+		int negativos = 0;
 
-	        for (double num : numeros) {
-	            maior = num > maior ? num : maior;
-	            menor = num < menor ? num : menor;
-	            positivos += num > 0 ? 1 : 0;
-	            negativos += num < 0 ? 1 : 0;
-	        }
+		System.out.println("Digite 4 números reais:");
+		for (int i = 0; i < 4; i++) {
+			numeros[i] = sc.nextDouble();
+		}
 
-	        System.out.println("Maior número: " + maior);
-	        System.out.println("Menor número: " + menor);
-	        System.out.println("Quantidade de positivos: " + positivos);
-	        System.out.println("Quantidade de negativos: " + negativos);
+		double maior = numeros[0];
+		double menor = numeros[0];
 
-	        sc.close();
-	    }
+		for (int i = 0; i < 4; i++) {
+			if (numeros[i] > maior) {
+				maior = numeros[i];
+			}
+			if (numeros[i] < menor) {
+				menor = numeros[i];
+			}
+			if (numeros[i] > 0) {
+				positivos++;
+			} else if (numeros[i] < 0) {
+				negativos++;
+			}
+		}
+
+		System.out.println("Maior número: " + maior);
+		System.out.println("Menor número: " + menor);
+		System.out.println("Quantidade de positivos: " + positivos);
+		System.out.println("Quantidade de negativos: " + negativos);
+
+		sc.close();
 	}
-
+}
 
 //Leia 4 números reais. Escreva o maior, o menor, quantos são positivos e quantos são negativos;
--- src/atividades/atividade15.java
+++ src/atividades/atividade15.java
@@ -1,5 +1,64 @@
 package atividades;
 
+import java.util.Scanner;
+
 public class atividade15 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite 10 números:");
+		int[] numeros = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+
+			numeros[i] = sc.nextInt();
+			for (int x = 0; x < i; x++) {
+				if (numeros[i] == numeros[x]) {
+					System.out.print("Já foi digitado, digite novamente outro numero: ");
+					numeros[i] = sc.nextInt();
+					x = -1;
+				}
+			}
+		}
+		print(numeros);
+		System.out.println("soma: " + soma(numeros));
+		media(numeros);
+		MenorMaior(numeros);
+		sc.close();
+	}
+
+	public static void print(int[] numeros) {
+		System.out.println("\nSeus números:");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(numeros[i]);
+		}
+	}
+	
+	public static void MenorMaior(int[] numeros) {
+		int maior = numeros[0];
+		int menor = numeros[0];
+		for (int x = 0; x < 10; x++) {
+			if (numeros[x] > maior) {
+				maior = numeros[x];
+			}
+			if (numeros[x] < menor) {
+				menor = numeros[x];
+			}
+		}
+		System.out.println("maior numero: " + maior);
+		System.out.println("menor numero: " + menor);
+
+	}
+	public static int soma(int [] numeros) {
+		int soma = 0;
+		for(int i = 0; i < 10; i++) {
+		soma += numeros[i];
+		}
+		return soma;
+	}
+	public static void media(int []numeros) {
+		double media = (double) soma(numeros) / 10;
+		System.out.println("media: "+ media);
+	}
 
 }
